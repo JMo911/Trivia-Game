fff06f9f0254c7d5fab058fcd1867d25c858f3bf
Jacob Moss <jacobmoss09@gmail.com>
2019-06-23 23:03:15 -0500

    json question object built

---
+++ assets/javascript/app.js
@@ -0,0 +1,72 @@
+var questions = {
+    "Question 1": {
+        "Question 1" : "How many step sisters does Cinderella have?",
+        "Answer 1" : "2",
+        "Distractors" :  [3,4,5]
+    },
+    "Question 2": {
+        "Question 2" : "What's the name of the bear from the Jungle Book?",
+        "Answer 2" : "Baloo",
+        "Distractors" :  ["Mowgli", "Blue", "Pascal"]
+    },
+    "Question 3": {
+        "Question 3" : "What's the name of the kingdom in Frozen?",
+        "Answer 3" : "Arendelle",
+        "Distractors" :  ["Ariel", "Ardendale","Airbnb"]
+    },
+    "Question 4": {
+        "Question 4" : "Which animal does Moana's grandmother have tattoed on her back?",
+        "Answer 4" : "Stingray",
+        "Distractors" :  ["Jellyfish","Starfish","Nemo"]
+    },
+    "Question 5": {
+        "Question 5" : "What kind of animals own the ice cream shop in Zootopia?",
+        "Answer 5" : "Elephants",
+        "Distractors" :  ["Cows","Rabbits","Sloths"]
+    },
+    "Question 6": {
+        "Question 6" : "What kind of animal is Meeko from Pocahontas",
+        "Answer 6" : "Raccoon",
+        "Distractors" :  ["Badger","Squirrel","Hummingbird"]
+    },
+    "Question 7": {
+        "Question 7" : "What's the name of Belle's horse?",
+        "Answer 7" : "Philippe",
+        "Distractors" :  ["Phil","Maximus","Pegasus"]
+    },
+    "Question 8": {
+        "Question 8" : "Which word does Ariel use for a fork?",
+        "Answer 8" : "Dinglehopper",
+        "Distractors" :  ["Whozit","Thingamajig","Pokey-stick"]
+    },
+    "Question 9": {
+        "Question 9" : "Which ride was not an opening day attraction at Disneyland in 1955?",
+        "Answer 9" : "Pinocchio's Daring Journey",
+        "Distractors" :  ["Space Mountain","Mickey's Grand Adventure","Peter Pan's Flight"]
+    },
+    "Question 10": {
+        "Question 10" : "What was the last attraction that Walt Disney personally supervised?",
+        "Answer 10" : "Pirate's of the Caribbean",
+        "Distractors" :  ["Haunted Mansion","Big Thunder Mountain Railroad","Harry Potter World"]
+    },
+    "Question 11": {
+        "Question 11" : "By the end of this year, how many themed lands will Disneyland have?",
+        "Answer 11" : "9",
+        "Distractors" :  [7,12,15]
+    },
+    "Question 12": {
+        "Question 12" : "What year did Tokyo Disneyland open?",
+        "Answer 12" : "1983",
+        "Distractors" :  [1971,1990,2005],
+        "factoid" : "it was the first international Disney park"
+    },
+    "Question 13": {
+        "Question 13" : "Which park does not have a Winnie the Pooh themed attraction?",
+        "Answer 13" : "Disneyland Paris",
+        "Distractors" :  ["Disneyland Hong Kong","Disneyland Tokyo", "Shanghai Disneyland"]
+    },
+    
+
+};
+
+console.log(questions);
