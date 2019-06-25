f2265355bfea3523c41ca5bec2213047fd054b9b
Jacob Moss <jacobmoss09@gmail.com>
2019-06-24 22:47:58 -0500

    image sources are dynamic per answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,84 +4,84 @@ var questions = {
         "Question" : "How many step sisters does Cinderella have?",
         "Answer" : "2",
         "Distractors" :  [3,4,5],
-        "image-source" : "assets/images/cinderellawithstepsisters.jpg"
+        "Image" : "assets/images/cinderellawithstepsisters.jpg"
     },
     "Question 2": {
         "Data-position": 1,
         "Question" : "What's the name of the bear from the Jungle Book?",
         "Answer" : "Baloo",
         "Distractors" :  ["Mowgli", "Blue", "Pascal"],
-        "image-source" : "assets/images/Baloo.jpg"
+        "Image" : "assets/images/Baloo.jpg"
     },
     "Question 3": {
         "Data-position": 2,
         "Question" : "What's the name of the kingdom in Frozen?",
         "Answer" : "Arendelle",
         "Distractors" :  ["Ariel", "Ardendale","Airbnb"],
-        "image-source" : "assets/images/Arendelle.png"
+        "Image" : "assets/images/Arendelle.png"
     },
     "Question 4": {
         "Data-position": 3,
         "Question" : "Which animal does Moana's grandmother have tattoed on her back?",
         "Answer" : "Stingray",
         "Distractors" :  ["Jellyfish","Starfish","Nemo"],
-        "image-source" : "assets/images/Moanagrandmatatt.gif"
+        "Image" : "assets/images/Moanagrandmatatt.gif"
     },
     "Question 5": {
         "Data-position": 4,
         "Question" : "What kind of animals own the ice cream shop in Zootopia?",
         "Answer" : "Elephants",
         "Distractors" :  ["Cows","Rabbits","Sloths"],
-        "image-source" : "assets/images/Elephanticecream.jpg"
+        "Image" : "assets/images/Elephanticecream.jpg"
     },
     "Question 6": {
         "Data-position": 5,
         "Question" : "What kind of animal is Meeko from Pocahontas",
         "Answer" : "Raccoon",
         "Distractors" :  ["Badger","Squirrel","Hummingbird"],
-        "image-source" : "assets/images/Meeko.png"
+        "Image" : "assets/images/Meeko.png"
     },
     "Question 7": {
         "Data-position": 6,
         "Question" : "What's the name of Belle's horse?",
         "Answer" : "Philippe",
         "Distractors" :  ["Phil","Maximus","Pegasus"],
-        "image-source" : "assets/images/Belleandphilippe.jpg"
+        "Image" : "assets/images/Belleandphilippe.jpg"
     },
     "Question 8": {
         "Data-position": 7,
         "Question" : "Which word does Ariel use for a fork?",
         "Answer" : "Dinglehopper",
         "Distractors" :  ["Whozit","Thingamajig","Pokey-stick"],
-        "image-source" : "assets/images/Arielwithfork.gif"
+        "Image" : "assets/images/Arielwithfork.gif"
     },
     "Question 9": {
         "Data-position": 8,
         "Question" : "Which ride was not an opening day attraction at Disneyland in 1955?",
         "Answer" : "Pinocchio's Daring Journey",
         "Distractors" :  ["Space Mountain","Mickey's Grand Adventure","Peter Pan's Flight"],
-        "image-source" : "assets/images/pinocchiosdaringjourney.jpeg"
+        "Image" : "assets/images/pinocchiosdaringjourney.jpeg"
     },
     "Question 10": {
         "Data-position": 9,
         "Question" : "What was the last attraction that Walt Disney personally supervised?",
         "Answer" : "Pirate's of the Caribbean",
         "Distractors" :  ["Haunted Mansion","Big Thunder Mountain Railroad","Harry Potter World"],
-        "image-source" : "assets/images/piratesofthecaribbean.jpg"
+        "Image" : "assets/images/piratesofthecaribbean.jpg"
     },
     "Question 11": {
         "Data-position": 10,
         "Question" : "By the end of this year, how many themed lands will Disneyland have?",
         "Answer" : "9",
         "Distractors" :  [7,12,15],
-        "image-source" : "assets/images/disneylandparkslayout.jpg"
+        "Image" : "assets/images/disneylandparkslayout.jpg"
     },
     "Question 12": {
         "Data-position": 11,
         "Question" : "What year did Tokyo Disneyland open?",
         "Answer" : "1983",
         "Distractors" :  [1971,1990,2005],
-        "image-source" : "assets/images/tokeydisneylandopening.jpg",
+        "Image" : "assets/images/tokeydisneylandopening.jpg",
         "factoid" : "it was the first international Disney park"
     },
     "Question 13": {
@@ -89,7 +89,7 @@ var questions = {
         "Question" : "Which park does not have a Winnie the Pooh themed attraction?",
         "Answer" : "Disneyland Paris",
         "Distractors" :  ["Disneyland Hong Kong","Disneyland Tokyo", "Shanghai Disneyland"],
-        "image-source" : "assets/images/winniethepooh.gif"
+        "Image" : "assets/images/winniethepooh.gif"
     },
     
 
@@ -99,7 +99,7 @@ var counter;
 var correctAnswers=0;
 var incorrectAnswers=0;
 var previousQuestionNumbers=[];
-
+var picsource;
 
 
 function fetchQuestions (){
@@ -139,11 +139,15 @@ function fetchQuestions (){
     // // APPEND QUESTION ITSELF
     // if (previousQuestionNumbers.indexOf(randomquestionnumber) !== -1 ) {
     $(".card-body").append(questions["Question " + randomquestionnumber].Question);
+    
+    //grab associated pic
+    picsource = questions["Question " + randomquestionnumber].Image;
     // APPEND KEYED RESPONSE
     var keyedResponse = $("<button>");
     keyedResponse.text(questions["Question " + randomquestionnumber].Answer);
     keyedResponse.addClass("keyedresponse responseoption");
     $(".card-body").append(keyedResponse);
+    
     // APPEND DISTRACTORS
     for (i=0;i<questions["Question " + randomquestionnumber].Distractors.length; i++) {
     var answerbuttons = $("<button>");
@@ -178,17 +182,17 @@ $(".card-body").delegate(":button", "click", function(){
         rightAnswer.text("Well done!");
         $(".card-body").append(rightAnswer);
         var photo=$("<img>");
-        photo.attr("src","assets/images/Elephanticecream.jpg");
+        photo.attr("src",picsource);
         $(".card-body").append(photo);
         correctAnswers = correctAnswers++;
-        // console.log($(this));
+        console.log($(this));
         
         setTimeout(fetchQuestions,5000);
   
     }
     else{fetchQuestions();}
     
-    //  fetchQuestions();
+    
 
 });
 
